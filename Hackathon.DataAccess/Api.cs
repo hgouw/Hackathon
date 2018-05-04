6d68e1820bcb6ce369f263b6a177e1c61d3f1fcc
Herman Gouw <hermangouw@gmail.com>
2018-05-04 16:35:25 +1000

    Added Api.cs and installed Newtonsoft.Json

---
+++ Hackathon.DataAccess/Api.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hackathon.DataAccess
+{
+    public class Api
+    {
+        public string GetData(string url)
+        {
+
+
+            return string.Empty;
+        }
+    }
+}
